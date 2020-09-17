486889a8ee94e0d4a613ffeb03bf470dd5de91ea
yuuki0114 <y.no0114@gmail.com>
2020-09-18 07:06:42 +0900

    inital commit

---
+++ action.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My First Web App</title>
+</head>
+<body>
+<h1>あなたの名前と生まれた月</h1>
+
+こんにちは、<?php echo $_POST['name'] ?>さん
+<br>
+あなたの生まれた月は、<?php echo $_POST['month'] ?>月です。
+
+
+</body>
+</html>
---
+++ namemonth.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My First Web App</title>
+</head>
+<body>
+<h1>My First Web App</h1>
+
+<form action="action.php" method="post">
+  名前：　<input type="text" name="name" />
+  生まれた月：　<input type="text" name="month" />
+  <input type="submit">
+</form>
+
+</body>
+</html>
